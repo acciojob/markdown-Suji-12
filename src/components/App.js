d581dc3835990ddc6b78c82b0936a5fda592daf5
Suji-12 <23csec23sujithas@gmail.com>
2026-01-04 20:44:29 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,11 @@
-import React, { useState, useEffect } from "react";
-import ReactMarkdown from "react-markdown";
+import React from "react";
+import MarkdownEditor from "./MarkdownEditor";
+import "../styles.css";
 
 function App() {
-  const [text, setText] = useState("# Hello world");
-  const [output, setOutput] = useState("");
-
-  useEffect(() => {
-    setOutput(text);
-  }, [text]);
-
   return (
     <div className="app">
-      <textarea
-        className="textarea"
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-      />
-
-      <div className="preview">
-        <ReactMarkdown>{output}</ReactMarkdown>
-      </div>
+      <MarkdownEditor />
     </div>
   );
 }
