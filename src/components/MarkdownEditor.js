333ffd8163c1cdf94f6453b4e51da2d020220f97
Suji-12 <23csec23sujithas@gmail.com>
2026-01-04 20:46:18 +0530

    Update MarkdownEditor.js

--- src/components/MarkdownEditor.js
+++ src/components/MarkdownEditor.js
@@ -1,1 +1,28 @@
+import React, { useState, useEffect } from "react";
+import ReactMarkdown from "react-markdown";
+
+function MarkdownEditor() {
+  const [markdown, setMarkdown] = useState("# Hello world");
+  const [preview, setPreview] = useState("");
+
+  useEffect(() => {
+    setPreview(markdown);
+  }, [markdown]);
+
+  return (
+    <>
+      <textarea
+        className="textarea"
+        value={markdown}
+        onChange={(e) => setMarkdown(e.target.value)}
+      />
+
+      <div className="preview">
+        <ReactMarkdown>{preview}</ReactMarkdown>
+      </div>
+    </>
+  );
+}
+
+export default MarkdownEditor;
 
